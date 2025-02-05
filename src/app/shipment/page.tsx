3ace2d535ec97c7247660178a68f9781473446b8
Khalid Ghani <khalidghani.333@gamil.com>
2025-02-06 04:44:31 +0500

    error resolve

--- src/app/shipment/page.tsx
+++ src/app/shipment/page.tsx
@@ -1,15 +1,27 @@
 import React from 'react'
-import ShipmentForm from '../components/shipmentForm'
 
-const Shipment = async() => {
-    const res = await fetch("http://localhost:3000/api/shipengine")
-    const mess = await res.json()
-    console.log(mess)
+const Shipment = () => {
   return (
-    <div>
-        <ShipmentForm/>
-    </div>
+    <div>Shipment</div>
   )
 }
 
 export default Shipment
+
+
+
+// import React from 'react'
+// import ShipmentForm from '../components/shipmentForm'
+
+// const Shipment = async() => {
+//     const res = await fetch("http://localhost:3000/api/shipengine")
+//     const mess = await res.json()
+//     console.log(mess)
+//   return (
+//     <div>
+//         <ShipmentForm/>
+//     </div>
+//   )
+// }
+
+// export default Shipment
