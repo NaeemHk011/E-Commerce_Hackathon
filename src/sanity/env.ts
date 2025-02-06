1488d6a573bd3665d151464286d056e363c52ef8
Khalid Ghani <khalidghani.333@gamil.com>
2025-02-06 18:35:51 +0500

    error resolve

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -13,7 +13,7 @@ export const projectId = assertValue(
 
 export const token = assertValue(
   "skFWUUDg214ubgfu24h4w8OwIcl0rjpdnXFE7l9ItEqzcip95SwEEVY31wIg8rAeB971WyDR02pnOFNYgQyTNTsUzc7PkofMs8i001XwCnN6hLkebtVL2eYTsQLBc2qBK0VyJZ98S355TYfQ8yLD5Hi2IEUIiXqYKf39fKY3d2YOhkWAjqqR",
-  'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
+  'Missing environment variable: SANITY_API_TOKEN'
 )
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
